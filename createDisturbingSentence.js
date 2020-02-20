1fa364f3f5fadd5eab10a0242df21fd17503b575
Vaibhav <vaibhavrevankar1995@gmail.com>
2020-02-20 22:27:58 +0530

    Added one more Comple messup method
    Example: "Hello World hope this code works as it is" => "hlLEO wloRD HPoE THIS cODe wkrOs as iT IS"

--- createDisturbingSentence.js
+++ createDisturbingSentence.js
@@ -39,4 +39,37 @@ class UpLowCaseSentence{
         }
         return messedUpSentence;
     }
+    
+    completeMessUp(sentence){
+        let messageArray = sentence.split(' ');
+        let messedUpSentence = '';
+        for(let m of messageArray){
+            const retValue = this.scrambleLetter(m);
+            const messUpWord = this.MessUpString(retValue);
+            messedUpSentence += messUpWord + ' ';
+        }
+        return messedUpSentence;
+    }
+
+    scrambleLetter(myString){
+        let retString = '';
+        let arr = new Array();
+        for(let m of myString){
+            arr.push(m);
+        }
+        if (arr.length < 3) {
+            return myString;
+        }
+        
+        for (let i = arr.length - 2; i > 1; --i) {
+            const j = 1 + Math.floor(Math.random() * i);
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+         
+        for(let a of arr){
+            retString += a;
+        }
+
+        return retString;
+    }
 }
